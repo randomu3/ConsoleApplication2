62bb9f3bc211f5b361624c532c0b8da9cc4fe7e1
randomu3 <demiz99@mail.ru>
2024-01-29 20:36:43 +0500

    init our program

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    unsigned int a, b;
+    cout << "Введите два числа." << endl;
+    cout << "Первое число: "; cin >> a;
+    cout << "Второе число: "; cin >> b;
+
+    // Обмен значениями с помощью побитовых операций
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    // Вывод результатов после обмена
+    cout << "После обмена:" << endl;
+    cout << "Первое число: " << a << endl;
+    cout << "Второе число: " << b << endl;
+
+    return 0;
+}
